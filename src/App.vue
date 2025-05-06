f52caa0740daa7f6ffa32a48e0070950df69f9fc
Peter Osusky <peter.osusky@kios.sk>
2025-05-06 14:46:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref(0)
+const rawHtml = ref('<span style="color: red">This should be red.</span>')
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+</template>
